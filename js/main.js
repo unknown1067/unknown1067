bb141b1a266642a6cb3605b32991de7e4b848bcd
unknown1067 <54977450+unknown1067@users.noreply.github.com>
2019-09-06 16:49:28 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,55 @@
+/* 함수 , 객체,
+   비동기 - 이벤트 드리븐 프로그래밍 (콜백, 프로미스, async/await);
+                                   호출스택, 이벤트루프,*실행 컨텍스트*;
+   프로토타입(자바랑 가장 큰 차이점) 
+
+*/
+
+document.write("외부스크립트이에여");
+
+var box
+box=100;
+box=30;
+document.write(box);
+
+var s
+s=100;
+var t=Number("500"); //"500" 500
+document.write(t);
+
+var tag="<h1>String </h1>";
+document.write(tag);
+
+var s="true"
+var t=10>=100;
+var k=Boolean("hello");
+
+var num=100;
+var str="자바스트립트";
+
+document.write(typeof num, "<br>");
+document.write(typeof str, "<br>");
+document.write(typeof null, "<br>");
+document.write(typeof 1, "<br>");
+
+var height=170;
+document.write(height, "<br>");
+
+var num1=15;
+var num2=2;
+var result;
+result=num1+num2;
+document.write(result, "<br>");
+result=num1-num2;
+document.write(result,"<br>");
+result=num1*num2;
+document.write(result,"<br>");
+result=num1/num2;
+document.write(result,"<br>");
+result=num1%num2;
+document.write(result,"<br>");
+
+var a, b, c;
+a=10 , b=20, c=30;
+console.log(a);
+
